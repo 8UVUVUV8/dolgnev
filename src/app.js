b6f0b370383767f6e19c6f845a380e5e9194d75d
8UVUVUV8 <sz.andras2400@gmail.com>
2023-03-10 10:45:24 +0100

    Kezdet

---
+++ src/app.js
@@ -0,0 +1,32 @@
+const tbody = document.querySelector('#tbody')
+
+var dolgozoLista = [
+    {
+        name: "Pisti", city: "szolnok", salary: "400", boriték: "200"
+    },
+    {
+        name: "Pityu", city: "bag", salary: "400", boriték: "300"
+    },
+    {
+        name: "István", city: "tura", salary: "400", boriték: "150"
+    },
+    {
+        name: "Istvánka", city: "Miskolc", salary: "400", boriték: "69"
+    },
+    {
+        name: "Isti", city: "Duranda", salary: "400", boriték: "310"
+    },
+    {
+        name: "Istu", city: "Dány", salary: "400", boriték: "400"
+    }
+]
+
+console.log(tbody)
+
+
+
+
+
+
+
+
